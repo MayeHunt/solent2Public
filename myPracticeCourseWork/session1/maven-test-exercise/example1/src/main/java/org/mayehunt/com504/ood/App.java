e483e3d3b379822db48cf10c2faec3d0e221348f
Maye Hunt <1huntg03@solent.ac.uk>
2021-09-29 16:49:40 +0100

    Week 1 work

---
+++ myPracticeCourseWork/session1/maven-test-exercise/example1/src/main/java/org/mayehunt/com504/ood/App.java
@@ -0,0 +1,13 @@
+package org.mayehunt.com504.ood;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
